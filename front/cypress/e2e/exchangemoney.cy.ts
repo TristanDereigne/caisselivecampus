22744ed24453dce2791229b8bcb5af597159a837
TristanDereigne <tristandereigne@gmail.com>
2025-10-30 15:14:38 +0100

    fix(test): correct bill and payment amounts in exchange test

--- front/cypress/e2e/exchangemoney.cy.ts
+++ front/cypress/e2e/exchangemoney.cy.ts
@@ -1,13 +1,13 @@
 describe('check exchange : Bill 10, Paid 20 -> should return one 10 bill ', () => {
   it.only('passes', () => {
     cy.visit('http://localhost:5173')
-    cy.get('#cyBillInput').type('20')
+    cy.get('#cyBillInput').type('10')
     cy.wait(500)
-    cy.get('#cy10EurBtn').click()
+    cy.get('#cy20EurBtn').click()
     cy.wait(500)
     cy.get('#cyValidateBtn').click()
     cy.wait(1500)
-    cy.get('.item').should('have.length', 15) // vérifier que l'ensemble des billets est présent avec leurs quantités
-    cy.get('.item').eq(9).should('contain', '10 * 1') // 10 euros * 1 billet
+    cy.get('.item').should('have.length', 15)
+    cy.get('.item').eq(9).should('contain', '10 * 1')
   })
 })
